33a96585aced730a43360e3578863338f8fc05c6
akshatg5 <131449376+akshatg5@users.noreply.github.com>
2023-05-30 18:04:00 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, jsonify
 from database import engine
 import json
-from database import load_clubs_from_db
+from database import load_clubs_from_db,load_club_from_db
 
 
 app = Flask(__name__)
@@ -25,6 +25,26 @@ def hello():
 def list_clubs():
     return jsonify(load_clubs_from_db())
 
+@app.route('/club/<id>')
+def show_club(id):
+    CLUB = load_club_from_db(id)
+    if not CLUB:
+        return render_template('notfound.html',club=CLUB),404
+    else:
+        return render_template('club-page.html',club=CLUB)
+
+@app.route("/clubs")
+def clubs_list():
+    CLUBS = load_clubs_from_db()
+    return render_template('club-list.html',clubs=CLUBS)
+
+@app.route('/aboutus')
+def about_us():
+    return render_template('aboutus.html')
+
+@app.route('/contactus')
+def contact_us():
+    return render_template('contactus.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
--- database.py
+++ database.py
@@ -27,3 +27,16 @@ def load_clubs_from_db():
         club_details.append(row._asdict())
     return club_details
 
+def load_club_from_db(id):
+    with engine.connect() as conn:
+        result = conn.execute(
+            text(f"select * from clubs WHERE id = {id}")
+        )
+        rows = result.all()
+        if len(rows) == 0:
+            return None
+        else:
+            return rows[0]._asdict()
+        
+    
+
