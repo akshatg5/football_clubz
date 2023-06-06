5e9a5000bb3b1323bd05ceabcca904a20e0f5b21
akshatg5 <131449376+akshatg5@users.noreply.github.com>
2023-06-06 13:58:09 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, jsonify,request
+from flask import Flask, render_template, jsonify,request,url_for,redirect
 from database import engine
 import json
-from database import load_clubs_from_db,load_club_from_db,add_interaction_to_db
+from database import load_clubs_from_db,load_club_from_db
+import pandas as pd
+import os.path
 
 
 app = Flask(__name__)
@@ -46,16 +48,58 @@ def about_us():
 def contact_us():
     return render_template('contactus.html')
 
+if not os.path.exists("polls.csv"):
+    structure = {
+        "p_id" : [],
+        "poll_question" : [],
+        "option1": [],
+        "option2": [],
+        "option3": [],
+        "option4": [],
+        "option5": [],
+        "votes1": [],
+        "votes2": [],
+        "votes3": [],
+        "votes4": [],
+        "votes5": []
+    }
+    
+    #converting the dictionary into a pandas dataframe, if the df does not already exists
+    pd.DataFrame(structure).set_index("p_id").to_csv("polls.csv")
+    
+#if the df already exists in the os then we just have to read it 
+polls_df = pd.read_csv("polls.csv").set_index("p_id")
+
+@app.route('/home_polls')
+def home_polls(): # the index page for polling web app
+    return render_template('home_polls.html',polls=polls_df)
+
+@app.route('/polls/<p_id>')
+def polls(p_id):
+    poll_details = polls_df.loc[int(p_id)]
+    return render_template('show_poll.html',poll=poll_details)
+
+@app.route('/vote/<p_id>/<option>')
+def vote(p_id,option):
+    polls_df.at[int(p_id),'votes'+str(option)] += 1
+    polls_df.to_csv("polls.csv")
+    poll_details = polls_df.loc[int(p_id)]
+    return render_template('show_poll.html',poll=poll_details)
+        
 @app.route('/interact')
 def interact():
     data = request.form
-    add_interaction_to_db(data)
     return render_template('interact.html')
+
+#making a similar csv file to save the users that have visited the site
+# if not os.path.exists("users.csv"):
+#     users_structure = {
+        
+#     }
 
 @app.route('/interact/entry',methods=['post'])
 def interact_to_club():
     data = request.form
-    add_interaction_to_db(data)
     return render_template('interaction_result.html',user_entry=data)
 
 if __name__ == "__main__":
